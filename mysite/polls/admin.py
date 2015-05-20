3dde611dd8f5a817136cdfc284a68d73bcc0d6a1
Jonathan Kempf <kempfjj@gmail.com>
2015-05-19 23:41:12 -0400

    Messing with admin options=Choice/Question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Choice, Question
+
+class ChoiceInline(admin.StackInline):
+    model   =   Choice
+    extra   +   3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -17,5 +18,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-
-# Create your models here.
