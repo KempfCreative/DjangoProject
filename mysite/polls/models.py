d5eb3874e85db2e61858b45f571713d4e238a036
Jonathan Kempf <kempfjj@gmail.com>
2015-05-21 23:47:52 -0400

    Added a date time filter

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -18,3 +18,8 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
